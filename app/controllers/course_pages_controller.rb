9717b8bce69e2d51553b63076eedaf93e885cbc8
HC008 <h.wk.cheng@gmail.com>
2013-10-03 21:52:57 -1000

    Create home page with Twitter Bootstrap layout

---
+++ app/controllers/course_pages_controller.rb
@@ -0,0 +1,4 @@
+class CoursePagesController < ApplicationController
+  def home
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 CourseAvailability::Application.routes.draw do
+  get "course_pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
