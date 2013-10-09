d03dc9cf8e0adf8c03e3525d06d8fa1e83465582
HC008 <h.wk.cheng@gmail.com>
2013-10-08 19:29:09 -1000

    Add tentative changes to home page, create help page, and add image (use later on)

--- app/controllers/course_pages_controller.rb
+++ app/controllers/course_pages_controller.rb
@@ -1,4 +1,7 @@
 class CoursePagesController < ApplicationController
   def home
   end
+  
+  def help
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 CourseAvailability::Application.routes.draw do
-  get "course_pages/home"
+  get 'course_pages/home'
+  get 'course_pages/help'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
