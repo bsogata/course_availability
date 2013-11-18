30d0ff7423507ce40520b35ac01eae9ff4e089eb
HC008 <h.wk.cheng@gmail.com>
2013-11-18 12:58:59 -1000

    Add some comments and changed how the select_tag works in Course List page.

--- app/mailers/course_mailer.rb
+++ app/mailers/course_mailer.rb
@@ -1,6 +1,9 @@
 class CourseMailer < ActionMailer::Base
   default from: "UH Course"
   
+  # Method to send out an welcome email to the user when they first create his/her account.
+  #
+  # Parameter: user - the user object created when the account is created.
   def welcome_email(user)
   	@user = user
   	mail(to: @user.email, subject: 'Welcome to UH Course Availability web application')
