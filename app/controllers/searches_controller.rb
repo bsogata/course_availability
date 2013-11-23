a1bffbbc99822b57da9283f50b3dc4c6fc536f47
Branden Ogata <branden.s.ogata@gmail.com>
2013-11-23 11:04:45 -1000

    Issue #015: Search now works for courses already in the database; this does not yet check for matches that are on the UH Course Availability site but not in the database.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,11 +2,15 @@ class SearchesController < ApplicationController
 
   def create
     # Actual search should be performed here
-    @courses = Course.search(params[:search])
-    render 'show'
+    puts "params[:search]: #{params[:search]}"
+    flash[:search] = params[:search]
+    redirect_to searches_path
   end
   
-  def show
+  def index
     # This will display the results of the search
+    puts "flash[:search]: #{flash[:search]}"
+    @courses = Course.search(flash[:search])
+    puts "Courses: #{@courses}"
   end
 end
