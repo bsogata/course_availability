491b63b301e76b617d409e0614aecf1e35eae95f
HC008 <h.wk.cheng@gmail.com>
2013-11-29 16:58:15 -1000

    Prototype to email notification code

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,3 +1,17 @@
 module UsersHelper
-  
+
+	def sending(user)
+		@user = user
+		@frequency_value = User.find(:frequency_value)
+		@frequency = User.find(:frequency)
+		
+		#Initial idea of testing which 
+		if @frequency == 'daily'
+		   CoursMailer.notify_email(@user)
+		elsif @frequency == 'min' || @frequency == 'hrs'
+			CoursMailer.notify_email(@user)
+		end
+		
+	end
+	
 end
