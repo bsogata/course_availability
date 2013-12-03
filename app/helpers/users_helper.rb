a35bba578fb59e53b43643b5cfcfb1bf533f57eb
HC008 <h.wk.cheng@gmail.com>
2013-12-02 14:55:42 -1000

    Issue #13 modify certain variables to actually grab the paritcular user's attribute instead of finding all of the users with a certain attribute.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,32 +2,36 @@ module UsersHelper
 
 	def self.perform(user)
 		@user = user
-		@frequency_value = User.find(:frequency_value)
-		@frequency = User.find(:frequency)
+		@frequent_value = @user.frequency_value
+		@frequent = @user.frequency
 		
 		#Initial idea of testing if it is daily selection
-		if @frequency == 'daily'
+		if @frequent == 'daily'
 			if Time.now == Time.beginning_of_day
 		   		CoursMailer.notify_email(@user)
-		   		Resque.enqueue(Time.beginning_of_day(), User, @frequency)
+		   		Resque.enqueue(Time.beginning_of_day(), @user, @frequenct)
 		   	end
 		# Minute selection
-		elsif @frequency == 'min'
+		elsif @frequent == 'min'
 			# Currently the closest function for finding the current minute and add the value 
 			# from the user
-		    @time = Time.now + @frequency_value.minutes
+		    @time = Time.now + @frequent_value.minutes
 		    
 		    if Time.now == @time
+		    	# The part to send out email, might also have to put in another variable to grab
+		    	# the courses the user has tracked.
 				CoursMailer.notify_email(@user)
-				Resque.enqueue(@time, User, @frequency)
+				Resque.enqueue(@time, @user, @frequent)
 			end
 		# Hour selection
-		elsif @frequency == 'hr'
+		elsif @frequent == 'hr'
 			# Currently the closest function for finding the current hour and add the value 
 			# from the user
-			@time_hours = Time.now + @frequency_value.hours
+			@time_hours = Time.now + @frequent_value.hours
 			
 			if Time.now == @time_hours 
+				# The part to send out email, might also have to put in another variable to grab
+		    	# the courses the user has tracked.
 				CoursMailer.notify_email(@user)
 				Resque.enqueue(@time_hours, User, @frequency)
 			end
