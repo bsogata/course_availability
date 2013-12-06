782acd0810cbdd4aed0c99209c354ae32aa0a76f
HC008 <h.wk.cheng@gmail.com>
2013-12-05 22:00:11 -1000

    Remove certain files that weren't needed as of now.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,31 +1,3 @@
 module UsersHelper
-
-	def self.perform(user)
-		@user = user
-		@frequent_value = @user.frequency_value
-		@frequent = @user.frequency
-		scheduler = Rufus::Scheduler.start_new
-		
-		#Initial idea of testing if it is daily selection
-		if @frequent == 'daily'
-			
-			scheduler.every(@frequent_value + "d") do
-				CourseMailer.notify_email(@user)
-		   	end
-		# Minute selection
-		elsif @frequent == 'min'
-		    
-			scheduler.every(@frequent_value + "m") do
-				CourseMailer.notify_email(@user)
-			end
-		# Hour selection
-		elsif @frequent == 'hr'
-			
-			scheduler.every(@frequent_value + "h") do
-				CourseMailer.notify_email(@user)
-			end
-		end
-		
-	end
 	
 end
