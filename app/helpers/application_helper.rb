5b16f5a724f5827b1d987773da8883746065c0ad
HC008 <h.wk.cheng@gmail.com>
2013-12-06 11:37:00 -1000

    Issue #13 Change a little format of the Welcome Email.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-	# Initial code for the initializer to be executed after everything is loaded
-	class Application < Rails::Application
-		config.after_initialize do
-			# Access each user's course and check if the seats changed
-			# If it did then schedule it to send out email based on a scheduler
-			# that is also based on the frequency of the user
-		end
 end
