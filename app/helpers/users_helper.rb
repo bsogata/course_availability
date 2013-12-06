a7206faa71815d586fc1cba54e1f14d013ef193f
HC008 <h.wk.cheng@gmail.com>
2013-12-06 11:45:18 -1000

    Issue #13 Adding back the rufus-scheduler code. This allows us to study how it works and think of how to change it.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,3 +1,29 @@
 module UsersHelper
-	
+		def self.perform(user)
+		@user = user
+		@frequent_value = @user.frequency_value
+		@frequent = @user.frequency
+		scheduler = Rufus::Scheduler.start_new
+		
+		#Initial idea of testing if it is daily selection
+		if @frequent == 'daily'
+			
+			scheduler.every(@frequent_value + "d") do
+				CourseMailer.notify_email(@user).deliver
+		   	end
+		# Minute selection
+		elsif @frequent == 'min'
+		    
+			scheduler.every(@frequent_value + "m") do
+				CourseMailer.notify_email(@user).deliver
+			end
+		# Hour selection
+		elsif @frequent == 'hr'
+			
+			scheduler.every(@frequent_value + "h") do
+				CourseMailer.notify_email(@user).deliver
+			end
+		end
+		
+	end
 end
