a50c563ddcb753faa251607539a11b6ebabe505e
Branden Ogata <branden.s.ogata@gmail.com>
2013-12-09 18:47:39 -1000

    Issue #035: Changes frequency_value to float.  Also fixes syntax error in CSS code.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131126231427) do
+ActiveRecord::Schema.define(version: 20131210042859) do
 
   create_table "course_lists", force: true do |t|
     t.string   "name"
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 20131126231427) do
     t.text     "email"
     t.text     "password_digest"
     t.string   "remember_token"
-    t.integer  "frequency_value"
+    t.float    "frequency_value"
     t.text     "frequency"
   end
 
